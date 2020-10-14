cfce06b8f473896f25e9b2e770011aa3a39b5e69
weizhenggang <503089877@qq.com>
2020-10-14 22:52:47 +0800

    对stage暂存表的结构进行改动，使其能反应汇编实验的进度

--- src/main/java/com/example/ustbdemo/Model/DataModel/Stage.java
+++ src/main/java/com/example/ustbdemo/Model/DataModel/Stage.java
@@ -8,6 +8,17 @@ import javax.persistence.*;
 
 /**
  * 暂存表，用于存储学生做汇编实验题时的进度，避免突然丢失
+ * tid: 题目id
+ * step：目前进入到第几阶段（1,2,3,4,5,6,7）        1:编写汇编代码，
+ *                                                 2：理想流水线 流水线视角仿真，
+ *                                                 3：理想流水线，指令视角仿真，
+ *                                                 4：冲关答题，
+ *                                                 5：实际流水线 流水线视角仿真，
+ *                                                 6：理想流水线 指令视角仿真，
+ *                                                 7：冲关答题
+ * code：学生写的代码
+ * simulatorStep:流水线的目前的周期数 ，即保存时流水线仿真运行到了哪个周期，整数
+ * tcid：当前选择题到了哪题
  */
 
 @Entity
@@ -29,14 +40,17 @@ public class Stage {
     @Column(name = "tcid")
     private Long tcid;          //选择题id
 
-    @Column(name = "simulator1_step")
-    private Long simulator1Step;    //模拟器1的步数
+    @Column(name = "simulator_step")
+    private Long simulatorStep;    //模拟器的步数
 
-    @Column(name = "simulator2_step")
-    private Long simulator2Step;    //模拟器2的步数
+//    @Column(name = "simulator2_step")
+//    private Long simulator2Step;    //模拟器2的步数
 
     @Column(name = "code")
     private String code;            //汇编代码
+
+    @Column(name = "step")
+    private Long step;          //当前进行的步骤
 
     public Long getStageId() {
         return stageId;
@@ -70,27 +84,27 @@ public class Stage {
         this.tcid = tcid;
     }
 
-    public Long getSimulator1Step() {
-        return simulator1Step;
-    }
-
-    public void setSimulator1Step(Long simulator1Step) {
-        this.simulator1Step = simulator1Step;
-    }
-
-    public Long getSimulator2Step() {
-        return simulator2Step;
-    }
-
-    public void setSimulator2Step(Long simulator2Step) {
-        this.simulator2Step = simulator2Step;
-    }
-
     public String getCode() {
         return code;
     }
 
     public void setCode(String code) {
         this.code = code;
+    }
+
+    public Long getSimulatorStep() {
+        return simulatorStep;
+    }
+
+    public void setSimulatorStep(Long simulatorStep) {
+        this.simulatorStep = simulatorStep;
+    }
+
+    public Long getStep() {
+        return step;
+    }
+
+    public void setStep(Long step) {
+        this.step = step;
     }
 }
