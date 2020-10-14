97bea63346f5679cfc3d39ef13d2a546809436c1
weizhenggang <503089877@qq.com>
2020-10-14 22:50:34 +0800

    新增了对Verilog代码提交次数的统计

---
+++ src/main/java/com/example/ustbdemo/Model/DataModel/VerilogRunTimes.java
@@ -0,0 +1,71 @@
+package com.example.ustbdemo.Model.DataModel;
+
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "verilog_run_times")        //记录verilog代码提交情况的数据库表
+@Data
+@ToString
+public class VerilogRunTimes {
+
+    @Id
+    @GeneratedValue
+    private Long timesId;       //主键
+
+    @Column(name = "tid")
+    private Long tid; // 题目id
+
+    @Column(name = "uid")
+    private Long uid; // 用户id
+
+    @Column(name = "times")
+    private Long times;  //学生提交次数
+
+    @Column(name = "update_at")
+    private Date updatedate;
+
+    public Long getTimesId() {
+        return timesId;
+    }
+
+    public void setTimesId(Long timesId) {
+        this.timesId = timesId;
+    }
+
+    public Long getTid() {
+        return tid;
+    }
+
+    public void setTid(Long tid) {
+        this.tid = tid;
+    }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public Long getTimes() {
+        return times;
+    }
+
+    public void setTimes(Long times) {
+        this.times = times;
+    }
+
+    public Date getUpdatedate() {
+        return updatedate;
+    }
+
+    public void setUpdatedate(Date updatedate) {
+        this.updatedate = updatedate;
+    }
+}
---
+++ src/main/java/com/example/ustbdemo/Repository/VerilogRunTimesRepository.java
@@ -0,0 +1,7 @@
+package com.example.ustbdemo.Repository;
+
+import com.example.ustbdemo.Model.DataModel.VerilogRunTimes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerilogRunTimesRepository extends JpaRepository<VerilogRunTimes,Long> {
+}
