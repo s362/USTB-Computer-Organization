e758a5c0cbd5bb9cb1d96de2f9eb91924c5e9788
weizhenggang <503089877@qq.com>
2020-11-01 22:18:29 +0800

    新增了直接向数据库中导入学生的方法

--- src/test/java/com/example/ustbdemo/MysqlDataChangeTest.java
+++ src/test/java/com/example/ustbdemo/MysqlDataChangeTest.java
@@ -2,13 +2,20 @@ package com.example.ustbdemo;
 
 
 import com.example.ustbdemo.Model.DataModel.Task;
+import com.example.ustbdemo.Model.DataModel.User;
 import com.example.ustbdemo.Service.TaskService;
+import com.example.ustbdemo.Service.UserService;
+import com.sun.xml.internal.fastinfoset.tools.FI_DOM_Or_XML_DOM_SAX_SAXEvent;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 //@RunWith(SpringRunner.class)
@@ -39,6 +46,52 @@ public class MysqlDataChangeTest {
 ////            System.out.println(item.getTdis());
 //
 //        }
+//
+//    }
+//    @Autowired
+//    UserService userService;
+//
+//    @Test
+//    public void addUser(){
+//
+//        File file=new File("e:\\vscode-workspace\\python\\184.txt");
+//        BufferedReader reader = null;
+//        try {
+//            System.out.println("以行为单位读取文件内容，一次读一整行：");
+//            reader = new BufferedReader(new FileReader(file));
+//            String tempString = null;
+//            int line = 1;
+//            // 一次读入一行，直到读入null为文件结束
+//            while ((tempString = reader.readLine()) != null) {
+//                // 显示行号
+//                System.out.println("line " + line + ": " + tempString);
+//                line++;
+//
+//
+//
+//                User xx=userService.findByUserName(tempString);
+//                if (xx==null){
+//                    User user=new User();
+//                    user.setUtype(2L);   //学生权限
+//                    user.setUsername(tempString);
+//                    user.setPasswd(tempString);
+//                    userService.addUser(user);
+//                }
+//
+//
+//            }
+//            reader.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (reader != null) {
+//                try {
+//                    reader.close();
+//                } catch (IOException e1) {
+//                }
+//            }
+//        }
+//
 //
 //    }
 }
